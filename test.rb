510869b9c69fa0bb42c0e0232ac9af04bee67110
Gunjan Patel <jalsamn@gmail.com>
2015-01-27 06:50:37 +0000

    d

--- test.rb
+++ test.rb
@@ -1,44 +1,4 @@
-File.delete('RevProducts.json')
-result = system("wget --header 'API-AUTHENTICATION: 288c79b9556747d6a66da933720b484a:aecdb2f39e8a4499980dd98a2f083856c547129de799420fa80ed302f51608cd' 'https://lotusmarket.revelup.com/resources/Product/?format=json&limit=5000000' -O 'RevProducts.json'")
-if result.nil?
-  puts "Error was #{$?}"
-elsif result
-  puts "Grabbed the JSON from Revel!"
-end
-
-json = File.new('RevProducts.json', 'r')
-parser = Yajl::Parser.new
-hash = parser.parse(json)
-
-
-  hash["objects"].each do |prod|
-
-    if !Product.exists?(:sku => prod["sku"])
-      product = Product.new
-      product.name = prod["name"]
-      product.sku = prod["sku"]
-      product.barcode = prod["barcode"]
-      product.category = prod["category"]
-      product.rewardpoint = prod["point_value"]
-      product.cost = prod["cost"]
-      product.active = prod["active"]
-      product.price = prod["price"]
-      product.save
-      puts "Added product "
-      puts prod["name"]
-    else
-      product = Product.find_by sku: prod["sku"]
-      product.name = prod["name"]
-      product.sku = prod["sku"]
-      product.barcode = prod["barcode"]
-      product.category = prod["category"]
-      product.rewardpoint = prod["point_value"]
-      product.cost = prod["cost"]
-      product.active = prod["active"]
-      product.price = prod["price"]
-      product.save
-      puts "Updated product "
-      puts prod["name"] 
-    end  
-  end
-
+puts "Hello world"
+puts "Hello world"
+puts "Hello world"
+puts "Hello world"
