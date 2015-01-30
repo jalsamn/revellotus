07a03972283105c9b6dad9e724df9c8b16af287d
Gunjan Patel <jalsamn@gmail.com>
2015-01-30 08:20:29 +0000

    s

--- app/grids/allsales_grid.rb
+++ app/grids/allsales_grid.rb
@@ -3,16 +3,15 @@ class AllsalesGrid
   include Datagrid
 
   scope do
-    Orderitem.select("productid, sum(actualqty) as TotalQty").group("productid", "actualqty")
+    Orderitem.select("productid, SUM(actualqty) as totalsold" )
+  .group("productid").order("totalsold")
+
   end
 
   filter(:productid, :integer)
   filter(:created_date, :date, :range => true)
 
- 
   column(:productid)
-  column(:TotalQty)
-   column(:finalsold, :header => "Sortable Sum", :order => "TotalQty") do
-    self.TotalQty
-  end
+  column(:totalsold)
+  
 end
