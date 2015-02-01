8b94c40db155be3c634c9b502bb57c08eb9bd0d1
Gunjan Patel <jalsamn@gmail.com>
2015-02-01 00:37:43 +0000

    s

--- db/migrate/20150131235832_createviewgroupedtls.rb
+++ db/migrate/20150131235832_createviewgroupedtls.rb
@@ -2,7 +2,6 @@ class Createviewgroupedtls < ActiveRecord::Migration
   def change
    
     execute %Q{
-        
        CREATE VIEW groupedtotals AS
        SELECT orderitems.productid, orderitems.created_date, products.name, sum(orderitems.actualqty) AS totalsold
        FROM orderitems, products
