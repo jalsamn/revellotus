db0e88c380018d9fc90439f91eae2b01e67d3f40
Gunjan Patel <jalsamn@gmail.com>
2015-02-09 22:33:10 +0000

    Complete revel daily import update scripts

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,13 @@
 
 # Learn more: http://github.com/javan/whenever
 
+every :day, :at => '11:00am' do
+  command "echo \"I am running from cron for daily revel order import!"
+  runner "scripts/dailyorderitemfetch.rb"
+  set :output, '/var/www/apps/12851/current/log/custom_deploy_after_migrate.log'
+end
+
+
 
 every :day, :at => '12:50am' do
   command "echo \"I am running from cron for revel import!"
--- scripts/dailyorderitemfetch.rb
+++ scripts/dailyorderitemfetch.rb
@@ -1,5 +1,11 @@
+time1    = Date.yesterday
+time2 = Time.new
 
-result = system("wget --header 'API-AUTHENTICATION: 288c79b9556747d6a66da933720b484a:aecdb2f39e8a4499980dd98a2f083856c547129de799420fa80ed302f51608cd' 'https://lotusmarket.revelup.com/resources/OrderItem/?format=json&limit=5000000000&updated_date__gte=2014-09-19&updated_date__lt=2014-11-21' -O 'scripts/RevOrderItems.json' --timeout=6000")
+address = "https://lotusmarket.revelup.com/resources/OrderItem/?format=json&limit=5000000000&updated_date__gte=" << time1.year.to_s << "-" << time1.month.to_s << "-" << time1.day.to_s << "&updated_date__lt=" << time2.year.to_s << "-" << time2.month.to_s << "-" << time2.day.to_s
+
+result = system("wget --header 'API-AUTHENTICATION: 288c79b9556747d6a66da933720b484a:aecdb2f39e8a4499980dd98a2f083856c547129de799420fa80ed302f51608cd' '#{address}' -O 'scripts/RevOrderItems.json' --timeout=6000")
+
+
 if result.nil?
   puts "Error was #{$?}"
 elsif result
@@ -15,11 +21,14 @@ hash = parser.parse(json)
 
   hash["objects"].each do |oi|
     begin
-      
+    
         orderitem = Orderitem.new
         orderitem.cost = oi["cost"]
         orderitem.created_by = oi["created_by"]
-        orderitem.created_date = oi["created_date"]
+        crtddate = oi["created_date"]
+        newcrtddate = crtddate.scan(/^.{0,10}/)
+        newcrtddate1 = newcrtddate.join()
+        orderitem.created_date = newcrtddate1
         orderitem.rev_id = oi["id"]
         orderitem.order_local_id = oi["order_local_id"]
         str =  oi["product"].to_s
@@ -33,17 +42,19 @@ hash = parser.parse(json)
         orderitem.revquantity = oi["quantity"]
         deca = oi["price"]
         decb = oi["initial_price"]
+    
         orderitem.actualqty = (deca.to_f / decb.to_f)
-        orderitem.updated_date = oi["updated_date"]
+        upddate = oi["updated_date"]
+        newupddate = upddate.scan(/^.{0,10}/)
+        newupddate1 = newupddate.join()
+        orderitem.updated_date = newupddate1
         orderitem.save
-        puts "added item "
-        puts oi["product_name_override"]
-        
+   
+    
+   
     rescue 
         $objArray.push "Error encountered @ " + oi["product_name_override"]
         puts "Error encountered @ " + oi["product_name_override"]
     end
-    
+  
   end
-#ErrorMailer.sample_email($objArray).deliver
-
