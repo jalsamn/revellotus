2b19da2db6c94eefeb62899b12ad75a6f9e6ea2b
Gunjan Patel <jalsamn@gmail.com>
2015-02-16 16:17:27 +0000

    s

---
+++ config/initializers/smtp_settings.rb
@@ -0,0 +1,8 @@
+  ActionMailer::Base.smtp_settings = {
+    :port           => 587,
+    :address        => "smtp.mailgun.org",
+    :domain         => ENV['domain'],
+    :user_name      => ENV['username'],
+    :password       => ENV['password'],
+    :authentication => :plain,
+  }
