d6f3c62fc585efd42f8ec8f1381fc23cf33f17a4
Gunjan Patel <jalsamn@gmail.com>
2015-02-16 16:11:54 +0000

    s

--- config/initializers/smtp_settings.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address        => 'smtp.gmail.com',
-    :port           => 587,
-    :domain         => 'gmail.com',
-    :authentication => :plain,
-    :user_name      => 'jalsamn@gmail.com',
-    :password       => 'Krishn@1'
-  }
