8492a863129bcacd9fa5e03c45bee3121346a9d3
Gunjan Patel <jalsamn@gmail.com>
2015-02-16 16:53:13 +0000

    s

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,12 +83,13 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
-ActionMailer::Base.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
   :port           => 587,
   :address        => "smtp.mailgun.org",
   :domain         => ENV['domain'],
   :user_name      => ENV['username'],
   :password       => ENV['password'],
   :authentication => :plain,
-}
+  }
+  
 end
