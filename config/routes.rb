b25180a1f37e0c0d5ce8934f1f374d454c2865fe
Gunjan Patel <jalsamn@gmail.com>
2015-06-13 23:39:46 +0000

    Changed stuff for phx templabels api

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,12 @@ Rails.application.routes.draw do
     scope module: :v1 do
       # We are going to list our resources here
       resources :users, :only => [:show]
+      #FOR MESA
       resources :templabels, :only => [:index, :create, :show]
       get 'csv', to: 'templabels#spitcsv'
+      #FOR Phoenix
+      resources :phxtemplabels, :only => [:index, :create, :show]
+      get 'phxcsv', to: 'templabels#spitcsv'
     end
   end
   
