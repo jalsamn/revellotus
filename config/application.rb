4c6618fe2dc83ed3d923e6d39eaad7375bd405fd
Gunjan Patel <jalsamn@gmail.com>
2015-06-14 00:09:55 +0000

    Fixed some api config

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,9 @@ module Revellotus
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    
+    config.api_only = false
+
+
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
