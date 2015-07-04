09fed3aee4f33d73cd7d142d67aac5b2db01e841
Gunjan Patel <jalsamn@gmail.com>
2015-07-04 22:58:53 +0000

    s

--- db/migrate/20150704222651_modifyview_groupedrevenuetotals1.rb
+++ db/migrate/20150704222651_modifyview_groupedrevenuetotals1.rb
@@ -2,7 +2,7 @@ class ModifyviewGroupedrevenuetotals < ActiveRecord::Migration
   def change
      execute %Q{
        CREATE OR REPLACE VIEW groupedrevenuetotals AS
-       SELECT orderitems.productid, orderitems.created_date, products.name, sum(orderitems.actualqty)*orderitems.price AS itemrevenue, products.category
+SELECT orderitems.productid, orderitems.created_date, products.name, sum(orderitems.pure_sales) AS itemrevenue, products.category
        FROM orderitems, products
        WHERE orderitems.productid = products.revid
        GROUP BY orderitems.productid, orderitems.created_date, products.name, products.category, orderitems.price
