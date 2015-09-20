9f3cf0378740d97300a2eacc849ac59d3b9d0d10
Gunjan Patel <gpatel@janakfoods.com>
2015-09-20 23:33:34 +0000

    S

--- test.rb
+++ test.rb
@@ -1,8 +1,10 @@
-def send_simple_message
-  RestClient.post "https://api:key-81696eb8ef4d7ffc862c6158a33598e2"\
-  "@api.mailgun.net/v2/sandbox991a7c03225a4cbb9f3a0f0310b71bb9.mailgun.org/messages",
-  :from => "Mailgun Sandbox <postmaster@sandbox991a7c03225a4cbb9f3a0f0310b71bb9.mailgun.org>",
-  :to => "Gunjan <jalsamn@gmail.com>",
-  :subject => "Hello Gunjan",
-  :text => "Congratulations Gunjan, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free."
-end
+@date1 = 999.days.ago 
+@date2 = Date.today
+
+@psale = Groupedtotal
+                .select("name, SUM(totalsold) as totalsold, productid")
+                .where(created_date: @date1..@date2)
+                .where(productid: '6384')
+                .group("name, productid")
+
+puts @psale.first.totalsold
